b2bbf103e12fa2ab7d14fc9ca5c243395eb4675a
Dibya ranjan Nayak <71962476+Dibyaranjan450@users.noreply.github.com>
2024-11-25 21:07:17 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,9 +64,7 @@ exports.protected = async (req, res, next) => {
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return next(
-        new AppError('You do not have permission to perform this action', 403)
-      );
+      return next( new AppError('You do not have permission to perform this action', 403));
     }
 
     next();
