a0ad5ad8f2dafe848770572888066ea547aa1023
dibyaranjan <dibyaranjan.nayak@myn.global>
2024-11-26 19:29:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(`${__dirname}/public`));
 
 app.use(express.json({ limit: '10kb' }));
-app.use(cors()              );
+app.use(cors());
 app.use(cookieParser());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
