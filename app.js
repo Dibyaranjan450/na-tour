d02085d3c2deadb30f55a6ecf9fe6723ddd1c5df
Dibyaranjan450 <dibyaranjan450@gmail.com>
2024-11-26 16:40:46 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorControlller');
@@ -19,6 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(`${__dirname}/public`));
 
 app.use(express.json({ limit: '10kb' }));
+app.use(cors());
 app.use(cookieParser());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
